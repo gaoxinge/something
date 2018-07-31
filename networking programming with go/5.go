3b7e62a4eac34a61a7b522e77583433f93a943fa
高辛格 <xg.gao@tianrang-inc.com>
2018-07-31 17:02:07 +0800

    networking programming with go

---
+++ networking programming with go/5.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "net"
+    "os"
+    "fmt"
+)
+
+func main() {
+    if len(os.Args) != 3 {
+        os.Exit(1)
+    }
+    
+    networkType := os.Args[1]
+    service := os.Args[2]
+    
+    port, err := net.LookupPort(networkType, service)
+    if err != nil {
+        fmt.Println("Error: ", err.Error())
+        os.Exit(2)
+    }
+    fmt.Println("Service port ", port)
+    os.Exit(0)
+}
