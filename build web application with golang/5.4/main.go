4e5348a7c026cdf3f9c3ecfccedd040b17ad1bb3
高辛格 <xg.gao@tianrang-inc.com>
2018-08-21 11:27:15 +0800

    build web application with golang

---
+++ build web application with golang/5.4/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+    "fmt"
+    "database/sql"
+    _ "github.com/lib/pq"
+)
+
+const (
+    host = ""
+    port = 0
+    user = ""
+    password = ""
+    dbname = ""
+)
+
+func main() {
+    psqInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=disable", 
+                            host, port, user, password, dbname)
+    db, err := sql.Open("postgres", psqInfo)
+    checkErr(err)
+    
+    rows, err := db.Query("SELECT id, crowdname, crowdvalue FROM zs_crowd_hash LIMIT 3")
+    checkErr(err)
+    
+    for rows.Next() {
+        var id int
+        var crowdname string
+        var crowdvalue int
+        
+        err = rows.Scan(&id, &crowdname, &crowdvalue)
+        checkErr(err)
+        fmt.Println(id, crowdname, crowdvalue)
+    }
+    
+    db.Close()
+}
+
+func checkErr(err error) {
+    if err != nil {
+        panic(err)
+    }
+}
