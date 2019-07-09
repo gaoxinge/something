f346ba6c55fbe61b0d3898e80612e5cfd37843a9
高辛格 <xg.gao@tianrang-inc.com>
2019-07-09 23:04:07 +0800

    cuda

---
+++ cuda/CUDA tutorial/vector_add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define N 1000000
+
+void vector_add(float *out, float *a, float *b, int n) {
+     for (int i = 0; i < n; i++) {
+         out[i] = a[i] + b[i];
+     }
+}
+
+int main() {
+     float *a, *b, *out;
+
+     a = (float *) malloc(sizeof(float) * N);
+     b = (float *) malloc(sizeof(float) * N);
+     out = (float *) malloc(sizeof(float) * N);
+
+     for (int i = 0; i < N; i++) {
+        a[i] = 1.0f;
+        b[i] = 2.0f;
+     }
+
+    vector_add(out, a, b, N);
+
+    //for (int i = 0; i < N; i++) {
+    //    printf("%0.1f ", out[i]);
+    //}
+
+    free(a);
+    free(b);
+    free(out);
+    return 0;
+}
---
+++ cuda/README.md
@@ -0,0 +1,9 @@
+# cuda
+
+## CUDA Tutorial
+
+- [CUDA Tutorial](https://cuda-tutorial.readthedocs.io/en/latest/)
+
+### code
+
+- [CUDA Tutorial]()
